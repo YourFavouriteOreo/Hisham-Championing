875a5ebfb9e312eab344e7114002ac371b54f783
YourFavouriteOreo <adiwani98@gmail.com>
2022-11-16 11:15:32 +0400

    switch from readline sync to prompt sync

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-const readline = require("readline-sync");
+const prompt = require("prompt-sync")();
 var input = ''
 var listDone = []
 var listUnDone = []
 var isDone = false
 while(!isDone){
     statusUpdate()
-    input = readline.question('what do you want to do?\n1.add\n2.remove\n3.change\n')
+    console.log('what do you want to do?\n1.add\n2.remove\n3.change\n')
+    input = prompt('')
     if(input === '1'){1
-        input = readline.question('Whatchu addin ombre?\n')
+        input = prompt('Whatchu addin ombre? ')
         listUnDone.push(input)
     }
     else if(input=== '2'){
-        input = readline.question('Whatchu removin ombre?\n')
+        input = prompt('Whatchu removin ombre? ')
          listUnDone.splice(Number(input),1)
     }
     else if(input=== '3'){
-        input = readline.question('Whatchu changin ombre?\n')
+        input = prompt('Whatchu changin ombre? ')
         var value = listUnDone[Number(input)]
         listDone.push(value)
         listUnDone.splice(Number(input),1)
